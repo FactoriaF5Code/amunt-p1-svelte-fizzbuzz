2a3c9e1a6d7cb732f6fbb6ed7896b05bfa050f71
Javi <javi@jsrois.net>
2023-05-17 17:20:46 +0200

    FizzBuzz with svelte (no tests)

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+    import NumberCard from './lib/Card.svelte'
+
+    let numbers = [...Array(100).keys()].slice(1)
+
+</script>
+
+<div>
+    {#each numbers as number}
+        <NumberCard {number}/>
+    {/each}
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+    }
+</style>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let number;
+    $: isMultipleOf2 = number % 2 === 0;
+    $: isMultipleOf3 = number % 3 === 0;
+</script>
+
+<div class:fizz="{isMultipleOf2}"
+     class:buzz="{isMultipleOf3}">
+    <h1>{number}</h1>
+    {#if isMultipleOf2}
+        <span>Fizz</span>
+    {/if}
+    {#if isMultipleOf3}
+        <span>Buzz</span>
+    {/if}
+</div>
+
+<style>
+    div {
+        background-color: #646cff;
+        width: 200px;
+        margin: 10px;
+        padding: 20px;
+        border-radius: 3px;
+        box-shadow: 5px 5px 2px 0 rgba(168, 70, 168, 1);
+    }
+
+    div:hover {
+        transform: translate(1px, 1px);
+    }
+
+    div.fizz {
+        background-color: #9ed6ff;
+    }
+
+    div.buzz {
+        background-color: #213547;
+    }
+
+    div.fizz.buzz {
+        background-color: #ff606d;
+    }
+
+</style>
